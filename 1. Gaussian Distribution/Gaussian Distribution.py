d5e09d027fedb73ca9a3668bb413127af0cf5222
Rahul Barhate <rahulbarhate97@gmail.com>
2018-02-06 18:57:14 +0530

    Update Gaussian Distribution.py

--- 1. Gaussian Distribution/Gaussian Distribution.py
+++ 1. Gaussian Distribution/Gaussian Distribution.py
@@ -7,6 +7,12 @@ s = np.random.normal(mu, sigma, 1000)
 
 #print(s)
 
+#Verifying the mean and the variance
+#abs(mu - np.mean(s)) < 0.01
+#abs(sigma - np.std(s, ddof=1)) < 0.01  // ddof=delta degrees of freedom
+                                        #to know more about ddof : https://stackoverflow.com/questions/27600207/why-does-numpy-std-give-a-different-result-to-matlab-std
+
+
 count, bins, ignored = plt.hist(s, 30, normed=True)
 plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
 plt.show()
